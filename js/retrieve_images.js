b9a59f4a9bf7e7b7be36750ca532919b22a9343c
James Choi <james.choiboi@gmail.com>
2012-02-16 15:19:44 -0500

    Implemented for PlayBook.

--- js/retrieve_images.js
+++ js/retrieve_images.js
@@ -7,30 +7,34 @@ var GIF_EXTENSION = ".gif";
 
 var HOME_DIR = "home/";
 var USER_DIR = "user/";
-var CAMERA_DIR = "camera/";
+var CAMERA_IN_DIR = "camera";
 var PICTURE_IN_DIR = "picture";
-var PHOTO_IN_DIR = "photos";
+var PHOTO_IN_DIR = "photo";
+var DOWNLOAD_IN_DIR = "download";
 
 var PB_ROOT_DIR = "file:///accounts/1000/shared/";
 var PB_CAMERA_DIR = "file:///accounts/1000/shared/camera/";
 var PB_PHOTO_DIR = "file:///accounts/1000/shared/photos/";
 var PB_DOWNLOADS_DIR = "file:///accounts/1000/shared/downloads/";
-var SMRT_ROOT_PHONE_DIR = "file:///";
-var SMRT_ROOT_SDCARD_DIR = "SDCard:///";
 
 /*
  * This function obtains the root directory and then call helper function which
  * recurses through sub-directories to locate directories containing images.
  */
-function getAllDirectories() {
-	if (blackberry.io.dir.exists(PB_ROOT_DIR)) {
-		var photoFiles =  blackberry.io.dir.listFiles(PB_DOWNLOADS_DIR);
-		alert(photoFiles.length)
-		getDirectories(PB_ROOT_DIR);
-	} else if (blackberry.io.dir.exists(SMRT_ROOT_PHONE_DIR)) {
-		getDirectories(SMRT_ROOT_PHONE_DIR);
-	} else if (blackberry.io.dir.exists(SMRT_ROOT_SDCARD_DIR)) {
-		getDirectories(SMRT_ROOT_SDCARD_DIR);
+function getAllDirectories() {	
+	try {
+		if (blackberry.io.dir.exists(PB_ROOT_DIR)) {
+			getDirectories(PB_CAMERA_DIR);
+			getDirectories(PB_DOWNLOADS_DIR);
+			getDirectories(PB_PHOTO_DIR);
+		}
+	} catch(e) {
+		var rootDir = blackberry.io.dir.getRootDirs(),
+			index = 0;
+			
+		for (index = 0; index < rootDir.length; index++) {
+			getDirectories(rootDir[index]);
+		}
 	}
 }
 
@@ -45,12 +49,14 @@ function getDirectories(dir) {
 		ind = 0;
 
 	checkImgDir(dir);
-	alert("getDirectories");
-	for (ind = 0; ind < dirs.length; ind++) {
-		alert("getDirectories: " = dirs[ind]);
-		if (dirs[ind] === CAMERA_DIR || dirs[ind] === HOME_DIR || dirs[ind] === USER_DIR || dirs[ind] === CAMERA_DIR ||
-				dirs[ind].toLowerCase().indexOf(PICTURE_IN_DIR) != -1 || dirs[ind].toLowerCase().indexOf(PHOTO_IN_DIR) != -1) { 
-			getDirectories(dir.concat(dirs[ind]));
+	
+	if (dir !== PB_DOWNLOADS_DIR) {
+		for (ind = 0; ind < dirs.length; ind++) {
+			if (dirs[ind] === HOME_DIR || dirs[ind] === USER_DIR || dirs[ind].toLowerCase().indexOf(CAMERA_IN_DIR) != -1 || 
+					dirs[ind].toLowerCase().indexOf(PICTURE_IN_DIR) != -1 || dirs[ind].toLowerCase().indexOf(PHOTO_IN_DIR) != -1 ||
+						dirs[ind].toLowerCase().indexOf(DOWNLOAD_IN_DIR) != -1) { 
+				getDirectories(dir.concat(dirs[ind]));
+			}
 		}
 	}
 }
